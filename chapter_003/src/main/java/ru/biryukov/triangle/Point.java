bfd6fb9f3278f69afb83463d927beb24b3a6d466
Stepan <Stepik0101@mail.ru>
2016-11-18 13:51:33 +0500

    Add files via upload

---
+++ chapter_003/src/main/java/ru/biryukov/triangle/Point.java
@@ -0,0 +1,15 @@
+﻿package triangle;
+
+public class Point {
+	public double x;
+	public double y;
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distanceTo(Point point) {
+		return Math.sqrt(Math.pow(x - this.x,2) + Math.pow(y - this.y,2));
+	}
+}
---
+++ chapter_003/src/main/java/ru/biryukov/triangle/Triangle.java
@@ -0,0 +1,18 @@
+package triangle;
+
+public class Triangle {
+	public Point a;
+	public Point b;
+	public Point c;
+	
+	public Triangle(Point a, Point b, Point c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double area() {
+		double perimeter = a + b + c;
+		return Math.sqrt(perimeter *(perimeter - a) * (perimeter - b) * (perimeter - c));
+	}
+}
