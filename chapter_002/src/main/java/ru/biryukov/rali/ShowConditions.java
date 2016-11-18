262adc3e6881f1ce5711771296fa4c83bafc256f
Stepan <Stepik0101@mail.ru>
2016-11-18 13:51:22 +0500

    Add files via upload

---
+++ chapter_002/src/main/java/ru/biryukov/rali/ShowConditions.java
@@ -0,0 +1,8 @@
+package rali;
+
+public class ShowConditions {
+	public static void main(String[] args) {
+		boolean condition = false;
+		System.out.println(condition ? "this is true" : "this is false");
+	}
+}
